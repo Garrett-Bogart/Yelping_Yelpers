6052428838e2c114c9a362a36a73714db8681ba0
Garrett Bogart <Garrett Bogart>
2017-04-11 15:29:37 -0600

    changed fileReader.py to a class called Business_Maker. It creates a dictionary of businesses id's and names

--- DataReader/fileReader.py
+++ DataReader/fileReader.py
@@ -1,43 +1,62 @@
-import json
+class Business_Maker:
 
-fin = open("yelp_dataset_challenge_round9.002", "r", encoding = "utf8")
-#count = 0
-finalData = []
-formated = []
-for line in fin:
-    temp = ""
-    for letter in line:
-        if letter.isalnum():
-            temp+=letter
-        else:
-            if letter ==",":
-                temp+=' '
-            if letter == "_":
-                temp+='_'
-            if letter == "-":
-                temp+='-'
-            if letter == ":":
-                temp+=':'
-    #if(count < 100):
-        #print(temp)
-    #count+=1
-    formated.append(temp)
+    def __init__(self):
+        business_dictionary = {}
 
-compressed_data = []
-count = 0
-for business in formated:
-   compress = business.split()
-   for word in compress:
-        temp = word.split(':')
-        for w in temp:
-            if w == 'business_id':
-                finalData.append(temp)
-            if w == 'state':
-                finalData.append(temp)
-        #if count < 100:
-            #print(temp)
-            #count +=1
+    def readFile(self,path):
+        fin = open(path, "r", encoding="utf8")
+        formated = []
+        for line in fin:
+            temp = ""
+            for letter in line:
+                if letter.isalnum():
+                    temp += letter
+                else:
+                    if letter == ",":
+                        temp += ' '
+                    if letter == "_":
+                        temp += '_'
+                    if letter == "-":
+                        temp += '-'
+                    if letter == ":":
+                        temp += ':'
+            formated.append(temp)
+        # print(formated)
+        return formated
 
-print(finalData)
-#print(formated)
+    def formating_one(self,final):
+        temp = []
+        for word in final:
+            temp.append(word[1])
+        return temp
 
+    def finalData(self,formated):
+        final = []
+        count = 0
+        for business in formated:
+            compress = business.split()
+            holder = []
+            for word in compress:
+                temp = word.split(':')
+                for w in temp:
+                    if w == 'business_id':
+                        holder.append(temp)
+                    if w == 'state':
+                        holder.append(temp)
+                    if w == 'name':
+                        holder.append(temp)
+            final.append(self.formating_one(holder))
+        return final
+
+    def make_dictionary(self,business_list):
+        business_index = {}
+        for business in business_list:
+            business_index[business[0]] = business[1]
+        return business_index
+
+    def make(self):
+        formated = self.readFile("Business_Subset.txt")
+        business_list = self.finalData(formated)
+        self.business_dictionary = self.make_dictionary(business_list)
+        print("business class")
+        print(self.business_dictionary)
